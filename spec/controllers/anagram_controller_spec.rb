f039b6957b2dac4305ef58772e493211a7364f44
rails-kung-foo <rails.kung.foo@gmail.com>
2015-06-21 11:59:04 +0100

    Order of test in controller#anagram normalized

--- spec/controllers/anagram_controller_spec.rb
+++ spec/controllers/anagram_controller_spec.rb
@@ -6,28 +6,25 @@ RSpec.describe AnagramController, :type => :controller do
       get :index
       expect(response).to be_success
     end
-  end
 
-  context 'search for angrams' do
-    it 'redirects to root path' do
-      get :index, search: 'time'
-
-      expect(response).to render_template("index")
-    end
-
-    context 'search anagrams' do
-      it 'that can be found' do
+    context 'search for angrams' do
+      it 'redirects to root path' do
         get :index, search: 'time'
-        expect(assigns(:result).count).to_not eq(0)
+        expect(response).to render_template("index")
       end
 
-      it 'that can\'t be found' do
-        get :index, search: 'animaltimer'
-        expect(assigns(:result).count).to eq(0)
+      context 'search anagrams' do
+        it 'that can be found' do
+          get :index, search: 'time'
+          expect(assigns(:result).count).to_not eq(0)
+        end
+
+        it 'that can\'t be found' do
+          get :index, search: 'animaltimer'
+          expect(assigns(:result).count).to eq(0)
+        end
       end
     end
   end
-
-
 
 end
